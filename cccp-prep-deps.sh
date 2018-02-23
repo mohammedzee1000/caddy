91fa0349a00311bb0722676143b55791fdef3da8
Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>
2018-02-23 13:56:23 +0530

    Adding script to prep deps in cccp prebuild.

---
+++ cccp-prep-deps.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+go get github.com/caddyserver/dnsproviders;
+go get github.com/caddyserver/dnsproviders/dnsimple;
+go get github.com/casbin/caddy-authz
+go get github.com/miquella/caddy-awses;
+go get github.com/miekg/caddy-prometheus;
